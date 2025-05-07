b304225171ecce51d28f065adafe30d81bd8def5
Itachi2311 <xuanan231103@gmail.com>
2025-05-07 19:21:50 +0700

    deleted

--- jobhive-back-main/src/main/java/tech/adrianmuntean/jobhive/model/ROLE.java
+++
@@ -1,5 +0,0 @@
-package tech.adrianmuntean.jobhive.model;
-
-public enum ROLE {
-    USER, ADMIN
-}
--- jobhive-back-main/src/main/java/tech/adrianmuntean/jobhive/model/STATUS.java
+++
@@ -1,5 +0,0 @@
-package tech.adrianmuntean.jobhive.model;
-
-public enum STATUS {
-    NEW, APPLIED, PRELIM_TEST, HR_CALL, TECH_INTERVIEW, HR_INTERVIEW, OFFER, HIRED, REJECTED, DROPPED, INACTIVE
-}
